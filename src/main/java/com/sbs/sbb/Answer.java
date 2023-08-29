e649a5a7db7dafea4779907d6d5485a60bf2356d
nowJun82 <oizill5481@icloud.com>
2023-08-29 13:38:17 +0900

    Question, Answer 엔티티 추가

---
+++ src/main/java/com/sbs/sbb/Answer.java
@@ -0,0 +1,24 @@
+package com.sbs.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity // answer 테이블
+public class Answer {
+    @Id // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT") // TEXT
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
